b606c638c9531909175ff598fa0f96dfe3cf74d6
周胜华 <zhoushenghua@163.com>
2020-07-28 16:02:26 +0800

    leetcode

---
+++ src/main/java/com/example/demo/leetCode/array/FirstMissingPositive.java
@@ -0,0 +1,26 @@
+package com.example.demo.leetCode.array;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 41.缺失的第一个正数
+ *   给你一个未排序的整数数组，请你找出其中没有出现的最小的正整数。
+ *   示例 1: 输入: [1,2,0]   输出: 3
+ *   示例 2: 输入: [3,4,-1,1]  输出: 2
+ *   示例 3: 输入: [7,8,9,11,12]  输出: 1
+ */
+public class FirstMissingPositive {
+    public int method(int[] nums){
+        int len = nums.length;
+        Set<Integer> hashSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        for (int i = 1; i <= len; i++) {
+            if (!hashSet.contains(i)) {
+                return i;
+            }
+        }
+        return len + 1;
+    }
+
+}
