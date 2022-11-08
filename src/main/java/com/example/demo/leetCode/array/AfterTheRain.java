054c3bdfc1ffb8e68160eb383eb7e8cd4a9f196a
zhoushenghua <zhoushenghua@meituan.com>
2022-11-08 19:51:36 +0800

    fix

--- src/main/java/com/example/demo/leetCode/array/AfterTheRain.java
+++ src/main/java/com/example/demo/leetCode/array/AfterTheRain.java
@@ -1,5 +1,8 @@
 package com.example.demo.leetCode.array;
 
+/**
+ * https://leetcode.cn/problems/trapping-rain-water/description/
+ */
 public class AfterTheRain {
 
     public static int trap(int[] height){
@@ -30,8 +33,24 @@ public class AfterTheRain {
         return sum;
     }
 
+    /**
+     * https://leetcode.cn/problems/trapping-rain-water/solutions/1322689/jie-yu-shui-by-ursmile-g0vp/
+     * @param height
+     * @return
+     */
+    public static int trap1(int[] height){
+        int res = 0, h1 = 0, h2 = 0;
+        for (int i = 0, j = height.length -  1; i < height.length && j >= 0; i++, j--) {
+            h1 = Math.max(h1, height[i]);
+            h2 = Math.max(h2, height[j]);
+            res += h1 + h2 - height[i];
+        }
+        return res - height.length * h1;
+    }
+
     public static void main(String[] args) {
         int[] height = new int[]{2,0,2};
         System.out.println(trap(height));
+        System.out.println(trap1(height));
     }
 }
